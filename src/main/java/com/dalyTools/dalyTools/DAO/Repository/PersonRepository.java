f605c4fc2e276f3929bb5cc59a5022537b4fd502
Borisov Alexey <borisovam@data-frame.ru>
2021-02-12 11:33:07 +0300

    change Person(Service/Repository)

--- src/main/java/com/dalyTools/dalyTools/DAO/Repository/PersonRepository.java
+++ src/main/java/com/dalyTools/dalyTools/DAO/Repository/PersonRepository.java
@@ -3,9 +3,11 @@ package com.dalyTools.dalyTools.DAO.Repository;
 import com.dalyTools.dalyTools.DAO.Entity.Person;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface PersonRepository extends JpaRepository<Person,Integer> {
 
     Person deleteByName(String name);
 
-    Person findByName(String name);
+    Optional<Person> findByUsername(String name);
 }
--- src/main/java/com/dalyTools/dalyTools/DAO/Service/PersonService.java
+++ src/main/java/com/dalyTools/dalyTools/DAO/Service/PersonService.java
@@ -1,17 +1,91 @@
 package com.dalyTools.dalyTools.DAO.Service;
 
 import com.dalyTools.dalyTools.DAO.Entity.Person;
+import com.dalyTools.dalyTools.DAO.Repository.PersonRepository;
+import com.dalyTools.dalyTools.DAO.Repository.RoleRepository;
+import com.dalyTools.dalyTools.DAO.dto.PersonDto;
+import com.sun.xml.bind.v2.TODO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
 
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.Optional;
+import java.util.UUID;
 
-public interface PersonService {
-    Person findByName(String name);
+@Service
+public class PersonService {
 
-    Person findById(int id);
+    private PersonRepository personRepository;
 
-    Person add(Person person);
+    private RoleRepository roleRepository;
 
-    Person deleteByName(String name);
+    @Autowired
+    public BCryptPasswordEncoder bCryptPasswordEncoder;
 
-    List<Person> findAll();
+
+    @Autowired
+    public PersonService(PersonRepository personRepository, RoleRepository roleRepository) {
+        this.personRepository = personRepository;
+        this.roleRepository = roleRepository;
+
+    }
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        BCryptPasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder();
+        return bCryptPasswordEncoder;
+    }
+
+    public Optional<Person> findById(int id) {
+        return personRepository.findById(id);
+    }
+
+
+    public Person add(Person person) {
+        return personRepository.save(person);
+    }
+
+    public Optional<Person> findByUserName(String name){return personRepository.findByUsername(toString());}
+
+
+    public Person deleteByName(String name) {
+        return personRepository.deleteByName(name);
+    }
+
+
+    public List<Person> findAll() {
+        return personRepository.findAll();
+    }
+
+
+    public Person registerNewPerson(PersonDto personDto){
+        Person registerPerson = new Person();
+        registerPerson.setName(personDto.getName());
+        registerPerson.setSername(personDto.getSername());
+        registerPerson.setUsername(personDto.getUsername());
+        registerPerson.setEmail(personDto.getEmail());
+        registerPerson.setActivationCode(UUID.randomUUID().toString());
+        registerPerson.setDataCreationCode(LocalDateTime.now());
+        registerPerson.setRole(roleRepository.findByName("ROLE_PERSON").orElseThrow(() -> { throw new NoSuchElementException("No such role found.");
+        }));
+        String encodedPassword = bCryptPasswordEncoder.encode(personDto.getPassword());
+        registerPerson.setPassword(encodedPassword);
+        //TODO: send email message
+
+
+       return personRepository.save(registerPerson);
+
+
+
+
+
+
+
+    }
+
 }

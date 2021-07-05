828bd9dd877bddd525b07c9c7584a885d1f9c8f1
Alexey <borisov_alexey_2002@mail.ru>
2021-07-05 23:25:09 +0300

    chenge sql function

--- src/test/java/com/dalyTools/dalyTools/DAO/Service/PersonServiceTest.java
+++ src/test/java/com/dalyTools/dalyTools/DAO/Service/PersonServiceTest.java
@@ -1,38 +1,22 @@
 package com.dalyTools.dalyTools.DAO.Service;
 
-import com.dalyTools.dalyTools.DAO.Repository.PersonRepository;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.Mockito;
-import org.mockito.junit.jupiter.MockitoExtension;
-
-import javax.persistence.Id;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.when;
 
-@ExtendWith(MockitoExtension.class)
+@SpringBootApplication
 class PersonServiceTest {
 
-    @InjectMocks
+    @Autowired
     PersonService personService;
 
-    @Mock
-    PersonRepository personRepository;
 
-    @Test
-    void findById() {
-        assertNotNull(personService);
-        when(personService.findById(3)).thenReturn(DaoTestData.aPerson());
-        assertEquals("ALEXEY",personService.findById(3).get().getUsername());
-    }
 
 
     @Test
-    void findAll() {
-        when(personService.findAll()).thenReturn(DaoTestData.aListOfPerson());
-        assertEquals(2,personService.findAll().size());
+    void findByUserName() {
+
     }
 }

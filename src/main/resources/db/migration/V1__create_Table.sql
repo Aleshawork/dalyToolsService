90732aa22968e4ad77e33a32107bab621d928486
Borisov Alexey <borisovam@data-frame.ru>
2021-08-25 22:10:35 +0300

    add migration script

--- src/main/resources/db/migration/V1__create_Table.sql
+++ src/main/resources/db/migration/V1__create_Table.sql
@@ -1,4 +1,3 @@
-
 CREATE TABLE IF NOT EXISTS refresh_token(
                                             id bigint,
                                             refresh_token varchar(255)
@@ -36,15 +35,3 @@ CREATE TABLE IF NOT EXISTS day_task(
                                        task varchar(250),
                                        priority int
 );
-
-INSERT INTO roles (id, name) VALUES
-(1, 'ROLE_PERSON'),
-(2, 'ROLE_ADMIN');
-
-INSERT INTO person (id, first_name, second_name,username, email, password,role_id) VALUES
-(1, 'Alexey', 'Borisov', 'AlexeyBorisov', 'borisov_am@mail.ru', '$2a$10$GN2V5I/dZSrHtZLrwiovYeHPHgD7kSU30ToIogMQ/RKSt4uOlpp8y',1),
-(2, 'Egor', 'Ivanov', 'IvanovY',  'ivanov@mail.ru', '$2y$12$6G7v5bApu2ZRH7W.HV8NHuJs0W4pDQ3xMmXZSW1k03Dfvzk78lkaO',2);
-
-
-
-
